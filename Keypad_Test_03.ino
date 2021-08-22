f8f7c51b23a5df5224ef73e3fae0ce19bb7bd9dd
Ethan Spratt <87547845+EthanSpratt@users.noreply.github.com>
2021-08-22 13:42:25 -0500

    Add files via upload

--- Keypad_Test_03.ino
+++ Keypad_Test_03.ino
@@ -14,8 +14,8 @@ byte rowPins[ROWS] = {5, 6, 7, 8}; //connect to the row pinouts of the keypad
 byte colPins[COLS] = {2, 3, 4}; //connect to the column pinouts of the keypad
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 
-const String password = "9819";
-String Input[4]="";
+char password [5] = "9819";
+char inputstr[5];
 
 LiquidCrystal lcd(12, 11, 14, 15, 16, 17);
 int LCD_POS = 0;
@@ -23,31 +23,43 @@ void setup(){
   Serial.begin(9600);
 
   lcd.begin(16, 2);
+  lcd.cursor();
   lcd.setCursor(0, 0);
   Serial.println("Press Any Key");
-  lcd.print("Press Any Key");
+  lcd.print("Enter Password");
 }
    
 void loop(){
   lcd.setCursor(LCD_POS,1);
   char key = keypad.getKey();
+  char keystr[2] = " ";
    
   if (key != NO_KEY){
     Serial.println(key);
-    strcat(Input, key);
-    Serial.println(Input);   
-    lcd.print(Input);
-    if(strlen(Input) == 4)
+    keystr[0] = key;
+    strcat(inputstr, keystr);
+    Serial.println(inputstr);   
+    lcd.print(inputstr);
+    
+    if(strlen(inputstr) == 4)
     {
-      if(strcmp(Input) == password)
+      if(strcmp(inputstr, password) == 0)
       {
         lcd.clear();
         lcd.print("SUCCESS");
+        inputstr[0] = '\0';
+        delay(1500);
+        lcd.setCursor(0, 0);
+        lcd.print("Enter Password");   
       }
       else
       {
         lcd.clear();
         lcd.print("FAILURE");
+        inputstr[0] = '\0';
+        delay(1500);
+        lcd.setCursor(0, 0);
+        lcd.print("Enter Password");          
       }
     }  
     
